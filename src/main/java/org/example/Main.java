15489a92857edb0faa894fd5257e32b0a5fb0bc2
aidankeefe2022 <aidankeefe486@gmail.com>
2023-10-25 16:01:52 -0600

    windslab is semi predicted

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,40 @@
 package org.example;
-
+import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         DateGetter dateGetter = new DateGetter();
-        dateGetter.insertNewData();
         Anaylsis anal = new Anaylsis();
-        System.out.println(anal.windLoaded("CookeCity"));
         WEB web = new WEB();
-        web.WebsiteInit();
+        int choice = 1;
+        while(choice >= 1 && choice <= 3) {
+            System.out.println("1. get new data \n" +
+                    "2. analysis \n" +
+                    "3. start web server \n " +
+                    "4. end \n");
+            choice = input.nextInt();
+            switch (choice) {
+                case (1):
+                    dateGetter.insertNewData();
+                    break;
+                case (2):
+                    System.out.println(anal.windLoaded("CookeCity"));
+                    break;
+                case (3):
+                    web.WebsiteInit();
+
+
+            }
+        }
+
+
+
+
+
+
     }
 
 
